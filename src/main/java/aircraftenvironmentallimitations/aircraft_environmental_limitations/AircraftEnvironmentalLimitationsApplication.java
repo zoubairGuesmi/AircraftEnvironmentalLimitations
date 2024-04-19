db1590a0b96c50a92b4986b1cac12b98ff6a48fb
zoubairGuesmi <zoubair.guesmi92@gmail.com>
2024-04-19 18:10:53 +0200

    Initial commit

---
+++ src/main/java/aircraftenvironmentallimitations/aircraft_environmental_limitations/AircraftEnvironmentalLimitationsApplication.java
@@ -0,0 +1,13 @@
+package aircraftenvironmentallimitations.aircraft_environmental_limitations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftEnvironmentalLimitationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AircraftEnvironmentalLimitationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aircraftenvironmentallimitations/aircraft_environmental_limitations/AircraftEnvironmentalLimitationsApplicationTests.java
@@ -0,0 +1,13 @@
+package aircraftenvironmentallimitations.aircraft_environmental_limitations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftEnvironmentalLimitationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
